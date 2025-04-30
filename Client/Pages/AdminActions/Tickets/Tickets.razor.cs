79e312377e261d9e7cb59ea1b902adffe37b3d81
kurwanator1337 <ktvcss@mail.ru>
2025-04-30 22:01:07 +0400

    Правка действий, страница тикетов для админов

---
+++ Client/Pages/AdminActions/Tickets/Tickets.razor.cs
@@ -0,0 +1,26 @@
+﻿using kTVCSS.Models.Db.Models.Tickets;
+using System.Net.Http.Json;
+
+namespace kTVCSSBlazor.Client.Pages.AdminActions.Tickets
+{
+    public partial class Tickets
+    {
+        private bool ready = false;
+        private List<Ticket> _tickets = [];
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                Task.Run(async () =>
+                {
+                    _tickets = await http.GetFromJsonAsync<List<Ticket>>("/api/admins/gettickets");
+
+                    ready = true;
+
+                    await InvokeAsync(StateHasChanged);
+                });
+            }
+        }
+    }
+}
