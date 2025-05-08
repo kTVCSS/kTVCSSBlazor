d201aafcf32ef57be3cb8f56c6eab9bfd427e69a
kurwanator1337 <ktvcss@mail.ru>
2025-05-08 19:34:30 +0400

    Динамичная подгрузка данных в FAQ

--- Client/Pages/FAQ/FAQ.razor.cs
+++ Client/Pages/FAQ/FAQ.razor.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Net.Http.Json;
+using System.Net.NetworkInformation;
+using kTVCSS.Models.Models;
 using Radzen;
 using Radzen.Blazor;
 
@@ -6,5 +8,19 @@ namespace kTVCSSBlazor.Client.Pages.FAQ
 {
     public partial class FAQ
     {
+        private List<FaqItem> items;
+        private bool ready = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Task.Run(async () =>
+            {
+                items = await http.GetFromJsonAsync<List<FaqItem>>("/api/faq");
+
+                ready = true;
+
+                await InvokeAsync(StateHasChanged);
+            });
+        }
     }
 }
