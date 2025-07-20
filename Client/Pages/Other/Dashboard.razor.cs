82504a5c807dd40ef67b2eab41c8ad92551767c0
kurwanator1337 <ktvcss@mail.ru>
2025-07-20 22:06:56 +0400

    Переделан дашборд под требования Инжектеда

--- Client/Pages/Other/Dashboard.razor.cs
+++ Client/Pages/Other/Dashboard.razor.cs
@@ -5,18 +5,37 @@ namespace kTVCSSBlazor.Client.Pages.Other
     public partial class Dashboard
     {
         private List<kTVCSS.Models.Models.Dashboard> data = [];
+        private List<kTVCSS.Models.Models.DashboardAdminLog> admins = [];
         private bool ready = false;
+        private int days = 7;
 
         protected override async Task OnInitializedAsync()
         {
             Task.Run(async () =>
             {
-                data = await http.GetFromJsonAsync<List<kTVCSS.Models.Models.Dashboard>>("/api/admins/getdashboardinfo");
+                data = await http.GetFromJsonAsync<List<kTVCSS.Models.Models.Dashboard>>("/api/admins/getdashboardinfo?days=" + days);
+
+                admins = await http.GetFromJsonAsync<List<kTVCSS.Models.Models.DashboardAdminLog>>("/api/admins/GetDashboardInfoAdmins?days=" + days);
 
                 ready = true;
 
                 await InvokeAsync(StateHasChanged);
             });
+        }
+
+        private async Task Apply()
+        {
+            ready = false;
+
+            await InvokeAsync(StateHasChanged);
+
+            data = await http.GetFromJsonAsync<List<kTVCSS.Models.Models.Dashboard>>("/api/admins/getdashboardinfo?days=" + days);
+
+            admins = await http.GetFromJsonAsync<List<kTVCSS.Models.Models.DashboardAdminLog>>("/api/admins/GetDashboardInfoAdmins?days=" + days);
+
+            ready = true;
+
+            await InvokeAsync(StateHasChanged);
         }
     }
 }
