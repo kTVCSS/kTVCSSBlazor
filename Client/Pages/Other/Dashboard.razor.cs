c5c1b2abe4ff6739e38e1556e50a3c4ffc459d7a
kurwanator1337 <ktvcss@mail.ru>
2025-07-20 16:52:32 +0400

    Добавлена страница дашборда для админов. Изменение навигации для мобилки ввиду отсутствия футера

---
+++ Client/Pages/Other/Dashboard.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+
+namespace kTVCSSBlazor.Client.Pages.Other
+{
+    public partial class Dashboard
+    {
+        private List<kTVCSS.Models.Models.Dashboard> data = [];
+        private bool ready = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Task.Run(async () =>
+            {
+                data = await http.GetFromJsonAsync<List<kTVCSS.Models.Models.Dashboard>>("/api/admins/getdashboardinfo");
+
+                ready = true;
+
+                await InvokeAsync(StateHasChanged);
+            });
+        }
+    }
+}
